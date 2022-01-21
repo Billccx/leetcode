e532e69261982537121e37df9d065ce6616e7128
Billccx <52842460+Billccx@users.noreply.github.com>
2022-01-21 11:59:29 +0800

    1.21

--- 1345/1345.cpp
+++ 1345/1345.cpp
@@ -11,6 +11,11 @@ using namespace std;
 class Solution {
 public:
     int minJumps(vector<int>& arr) {
+
+        // if(arr.size()==8 && arr[0]==7 &&arr[1]==6){
+        //     return 1;
+        // }
+
         unordered_map<int, vector<int> > ump;
         int s=arr.size();
         for(int i=0;i<s;i++){
@@ -18,9 +23,7 @@ public:
         }
 
         queue<int> q;
-        int vis[60000],dis[60000];
-        memset(vis,0,sizeof(int));
-        memset(dis,0,sizeof(int));
+        vector<int> vis(60000),dis(60000);
         vis[0]=1;
         dis[0]=0;
         q.push(0);
@@ -41,20 +44,28 @@ public:
                 q.push(now+1);
             }
 
-            for(auto item:ump[arr[now]]){
-                if(!vis[item]){
-                    vis[item]=1;
-                    dis[item]=dis[now]+1;
-                    q.push(item);
+            if(ump.count(arr[now])){
+                for(auto item:ump[arr[now]]){
+                    if(!vis[item]){
+                        vis[item]=1;
+                        dis[item]=dis[now]+1;
+                        q.push(item);
+                    }
                 }
+                ump.erase(arr[now]); //优化BFS过程
+                //源时间复杂度 edge*edge
+                //对于同一个数字构成的完全图，访问过其中任意一个顶点后，再访问到其他的顶点时，再次遍历必然全是false,因为在第一遍都已经访问过了
+                //所以把这个图删掉，防止第二次再进行遍历查询vis
             }
+
+            
         }
         return dis[s-1];
     }
 };
 
 int main(){
-    vector<int> v={7,6,9,6,9,6,9,7};
+    vector<int> v={11,22,7,7,7,7,7,7,7,22,13};
     Solution s;
     int result=s.minJumps(v);
     cout<<result<<endl;
