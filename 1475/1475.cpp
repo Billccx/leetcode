b87b231abc00f7ff996a18499ef3d3251f6960aa
Billccx <52842460+Billccx@users.noreply.github.com>
2022-09-01 14:34:33 +0800

    8.31

---
+++ 1475/1475.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#include <map>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> result;
+    stack<pair<int,int>> s;
+    vector<int> finalPrices(vector<int>& prices) {
+        int len=prices.size();
+        result.resize(len,0);
+        for(int i=0;i<len;i++){
+            while(!s.empty() && s.top().first>prices[i]){
+                pair<int,int> temp=s.top();
+                s.pop();
+                result[temp.second]=temp.first-prices[i];
+            }
+            s.push(make_pair(prices[i],i));
+        }
+
+        while(!s.empty()){
+            pair<int,int> temp=s.top();
+            result[temp.second]=temp.first;
+            s.pop();
+        }
+        return result;
+    }
+};
+
+int main(){
+    Solution s;
+    vector<int> prices={10,1,1,6};
+    vector<int> result=s.finalPrices(prices);
+    for(int i=0;i<result.size();i++) cout<<result[i]<<' ';
+    cout<<endl;
+}
