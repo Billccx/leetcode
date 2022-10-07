a3ade5d5cc1863f31e31d8714eb8723cee08f124
Billccx <billcuichenxi@163.com>
2022-10-07 22:00:58 +0800

    10.7

---
+++ goTour/图像.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"image"
+	"image/color"
+
+	"golang.org/x/tour/pic"
+)
+
+type Image struct{}
+
+func (i Image) ColorModel() color.Model {
+	return color.RGBAModel
+}
+
+func (i Image) Bounds() image.Rectangle {
+	r := image.Rect(0, 0, 200, 200)
+	return r
+}
+
+func (i Image) At(x, y int) color.Color {
+	v := uint8(x ^ y)
+	return color.RGBA{v, v, 255, 255}
+}
+
+func main() {
+	m := Image{}
+	pic.ShowImage(m)
+}
