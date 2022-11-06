904b4151840756c2958e997640153688453b393e
Billccx <52842460+Billccx@users.noreply.github.com>
2022-11-06 16:38:15 +0800

    11.6

---
+++ 算法作业/assignment1/三角形剖分.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cmath>
+using namespace std;
+
+
+void print(int start,int end, vector<vector<int>>& path){
+    int p=path[start][end];
+
+    if(abs(start-p)>1){
+        cout<<start+1<<"-"<<p+1<<endl;
+        if(abs(start-p)>2) print(start,p,path);
+    }
+    if(abs(end-p)>1){
+        cout<<p+1<<"-"<<end+1<<endl;
+        if(abs(end-p)>2) print(p,end,path);
+    }
+}
+
+int main(){
+    vector<vector<int>> v={
+        {0, 14, 25, 27, 10, 11, 24, 16},
+        {0, 0, 18, 15, 27, 28, 16, 14},
+        {0, 0, 0, 19, 14, 19, 16, 10},
+        {0, 0, 0, 0, 22, 23, 15, 14},
+        {0, 0, 0, 0, 0, 14, 13, 20},
+        {0, 0, 0, 0, 0, 0, 15, 18},
+        {0, 0, 0, 0, 0, 0, 0, 27},
+        {0, 0, 0, 0, 0, 0, 0, 0}
+    };
+
+    vector<vector<int>> dp(8,vector<int>(8));
+    vector<vector<int>> path(8,vector<int>(8));
+
+    int n=8;
+    for(int i=0;i<n-2;i++){
+        dp[i][i+2]=v[i][i+1]+v[i+1][i+2]+v[i][i+2];
+    }
+
+    for(int k=3;k<n;k++){
+        for(int start=0;start<n-k;start++){
+            int end=start+k;
+            dp[start][end]=INT_MAX;
+            for(int p=start+1;p<end;p++){
+                int temp=dp[start][p]+dp[p][end]+v[start][p]+v[p][end]+v[start][end];
+                if(temp<dp[start][end]){
+                    dp[start][end]=temp;
+                    path[start][end]=p;
+                } 
+            }
+        }
+    }
+
+    for(int i=0;i<8;i++){
+        for(int j=0;j<8;j++){
+            cout<<dp[i][j]<<' ';
+        }
+        cout<<endl;
+    }
+
+    cout<<"result:"<<dp[0][7]<<endl;
+    cout<<"plan:"<<endl;
+    print(0,7,path);
+    return 0;
+}
