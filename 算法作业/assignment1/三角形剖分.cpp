e15c885311dbfa18c80e08ae81ea78da7c4edb6d
Billccx <52842460+Billccx@users.noreply.github.com>
2022-11-09 09:45:15 +0800

    11.9

--- 算法作业/assignment1/三角形剖分.cpp
+++ 算法作业/assignment1/三角形剖分.cpp
@@ -5,7 +5,7 @@
 #include <cmath>
 using namespace std;
 
-
+//递归打印选择路径
 void print(int start,int end, vector<vector<int>>& path){
     int p=path[start][end];
 
@@ -35,10 +35,12 @@ int main(){
     vector<vector<int>> path(8,vector<int>(8));
 
     int n=8;
+    //初始化dp数组
     for(int i=0;i<n-2;i++){
         dp[i][i+2]=v[i][i+1]+v[i+1][i+2]+v[i][i+2];
     }
 
+    //动态规划推导
     for(int k=3;k<n;k++){
         for(int start=0;start<n-k;start++){
             int end=start+k;
@@ -53,15 +55,9 @@ int main(){
         }
     }
 
-    for(int i=0;i<8;i++){
-        for(int j=0;j<8;j++){
-            cout<<dp[i][j]<<' ';
-        }
-        cout<<endl;
-    }
-
-    cout<<"result:"<<dp[0][7]<<endl;
-    cout<<"plan:"<<endl;
+    //打印结果和选择路径
+    cout<<"最小权重之和:\n"<<dp[0][7]<<endl;
+    cout<<"弦的集合T:"<<endl;
     print(0,7,path);
     return 0;
 }
