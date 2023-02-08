18bb0b29ef64113afc5d02563bb8be0642995389
Billccx <52842460+Billccx@users.noreply.github.com>
2023-02-08 10:34:46 +0800

    2023.2.8

---
+++ 剑指2/008.和大于等于 target 的最短子数组/和大于等于 target 的最短子数组.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int ans=0;
+        int l=0,r=0,len=nums.size(),sum=0;
+        vector<int> presum;
+        for(int i=0;i<len;i++){
+            sum+=nums[i];
+            presum.push_back(sum);
+        }
+        while(r<len){
+            int s=presum[r]-presum[l]+nums[l];
+            if(s>=target){
+                if(ans==0) ans=r-l+1;
+                else ans=min(ans,r-l+1);
+                l++;
+                if(l>r) r=l;
+            }
+            else r++;
+        }
+        return ans;
+    }
+};
